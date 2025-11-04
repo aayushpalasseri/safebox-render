72a5e2f3352e54a09479804c6225373bbcc63526
Aayush P <aayushpalasseri@gmail.com>
2025-11-04 20:47:34 +0530

    Added unlock flag to API status endpoint

--- api/status.js
+++ api/status.js
@@ -1,3 +1,7 @@
-export default async function handler(req, res) {
-  return res.json({ status: "OK", time: new Date().toISOString() });
+export default function handler(req, res) {
+  res.status(200).json({
+    status: "OK",
+    unlock: true, // change to true when you want to unlock
+    time: new Date().toISOString(),
+  });
 }
